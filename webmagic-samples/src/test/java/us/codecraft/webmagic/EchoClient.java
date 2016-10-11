42d61411a0eeee02a1133991eaa341f612df836a
柳巍 <liuw@fangde.com>
2016-10-11 15:39:22 +0800

    网络编程练习

---
+++ webmagic-samples/src/test/java/us/codecraft/webmagic/EchoClient.java
@@ -0,0 +1,52 @@
+package us.codecraft.webmagic;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Created by Administrator on 2016/10/11.
+ */
+public class EchoClient {
+
+    private String host = "localhost";
+    private int port = 8000;
+    private Socket socket;
+
+    public EchoClient () throws IOException{
+        socket = new Socket(host,port);
+    }
+
+    private PrintWriter getWriter (Socket socket) throws  IOException{
+        OutputStream socketOut = socket.getOutputStream();
+        return new PrintWriter(socketOut,true);
+    }
+
+    private BufferedReader getReader (Socket socket) throws  IOException {
+        InputStream socketIn = socket.getInputStream();
+        return new BufferedReader(new InputStreamReader(socketIn));
+    }
+
+    public void talk () throws IOException{
+        try {
+            BufferedReader br = getReader(socket);
+            PrintWriter pw = getWriter(socket);
+            BufferedReader localReader = new BufferedReader(new InputStreamReader(System.in));
+            String msg = null;
+            while ((msg=localReader.readLine()) != null) {
+                pw.println(msg);
+                System.out.println(br.readLine());
+                if (msg.equals("bye")) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            socket.close();
+        }
+    }
+
+    public static void main (String[] args) throws IOException {
+        new EchoClient().talk();
+    }
+}
---
+++ webmagic-samples/src/test/java/us/codecraft/webmagic/EchoPlayer.java
@@ -0,0 +1,31 @@
+package us.codecraft.webmagic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Administrator on 2016/10/11.
+ */
+public class EchoPlayer {
+
+    public String echo (String msg){
+        return "echo:" + msg;
+    }
+
+    public void talk () throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String msg = null;
+        while((msg=br.readLine())!=null){
+            System.out.println(echo(msg));
+            if(msg.equals("byte")){
+                System.out.println("即将退出进程。。。");
+                break;
+            }
+        }
+    }
+
+    public static void main (String[] args) throws IOException {
+        new EchoPlayer().talk();
+    }
+}
